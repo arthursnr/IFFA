fb47f4a5fcfe46a4520a97cfe8af801b87a4d0f7
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-03-30 17:34:44 -0300

    first commit

---
+++ app/components/buttons/NavMenu.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { motion } from 'framer-motion'
+
+export function NavMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // toggle drawer
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+
+        <div className="lg:hidden">
+            
+            <button onClick={toggleDrawer} className="z-[999] relative">
+                {isOpen ? <X /> : <Menu />}
+            </button>
+
+            {/* Links do menu */}
+            <motion.div
+                initial={{ x: '-100%' }}
+                animate={{ x: isOpen ? '0%' : '-100%' }}
+                className="flex fixed left-0 right-0 top-20 overflow-y-auto h-full  bg-[#5E5EDA] backdrop-blur  text-white p-6">
+                <ul>
+                    <ul className="flex flex-col text-xl font-semibold">
+                        <li className="hover:text-gray-300 hover:bg-white/5 rounded-md cursor-pointer py-4 px-2"><a href="#" className="relative">Home Page</a></li>
+                        <li className="hover:text-gray-300 hover:bg-white/5 rounded-md cursor-pointer py-4 px-2"><a href="#" className="relative">Sobre Nós</a></li>
+                        <li className="hover:text-gray-300 hover:bg-white/5 rounded-md cursor-pointer py-4 px-2"><a href="#" className="relative">Como Apoiar</a></li>
+                    </ul>
+                </ul>
+            </motion.div>
+        </div>
+    );
+}
