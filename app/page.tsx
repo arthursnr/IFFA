ce5f9e1768813e55d02c5f50349c35bcf6121500
Arthur Ribeiro <115566700+arthursnr@users.noreply.github.com>
2026-05-21 14:13:28 -0300

    Add Head component with manifest and theme color

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import { MainPage } from "./components/mainPage/mainPage";
 export default function Home() {
 
   return (
+    <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+     </Head>
     <div className="min-h-screen min-w-screen bg-gray-100">
       <MainPage />
     </div>
