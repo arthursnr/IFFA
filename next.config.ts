536e45b441c43c96164fa3c7dfa72e6008bd0f1e
Arthur Ribeiro <arthursnr2021@gmail.com>
2026-05-21 22:04:05 -0300

    better changes

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-});
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
